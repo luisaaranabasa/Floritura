131bde33e00f4345edada951c03f7c95d965b20b
paula1612 <paulanieto1612@gmail.com>
2024-11-07 00:08:34 +0100

    estructura carrusel scroll obligatorio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // EFECTO DEMO ESCRIBIENDO
 
- if ($(".text-slider").length == 1) {
+if ($(".text-slider").length == 1) {
             
     var typed_strings = 
         $(".text-slider-items").text();
@@ -11,4 +11,27 @@
                 backDelay: 900,
                 backSpeed: 30,
             });
-        }
+}
+
+
+const spaceHolder = document.querySelectorAll('.space-holder');
+const horizontal = document.querySelectorAll('.horizontal');
+const container = document.querySelectorAll('.container');
+const sticky = document.querySelectorAll('.sticky');
+
+function calcDynamicHeight(ref) {
+  const vw = window.innerWidth;
+  const vh = window.innerHeight;
+  const objectWidth = ref.scrollWidth;
+  return objectWidth - vw + vh + 150;
+}
+
+container.forEach(function(current, i) {
+  spaceHolder[i].style.height = `${calcDynamicHeight(horizontal[i])}px`;
+    window.addEventListener('scroll', () => { 
+      horizontal[i].style.transform = `translateX(-${sticky[i].offsetTop}px)`;
+    });
+    window.addEventListener('resize', () => {
+      spaceHolder[i].style.height = `${calcDynamicHeight(horizontal[i])}px`;
+    });
+});
